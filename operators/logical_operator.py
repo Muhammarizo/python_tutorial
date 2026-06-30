5fd33cb6d74ce527f0f557a8c63b49b57343a6ce
muhammadrizo <muhammadrizo42maktab@gmail.com>
2026-06-30 12:05:36 +0500

    logical operator was added

---
+++ operators/logical_operator.py
@@ -0,0 +1,11 @@
+# Pythonda logical operatorlarni yozilishi boshqa tillardan biroz farq qiladi
+
+# and - agar ikkala statement ham togri bolsa true boladi
+# or - ikkalasidan bittasi true bolsa true boladi
+# not - teskari qiymatni qaytaradi
+
+x = 5
+print(x < 5 and x > 10)
+
+
+print(not(x > 3 and x < 10))
