4f334ea6f0af40f4c8cc18e75c7bbdaa3725de88
muhammadrizo <muhammadrizo42maktab@gmail.com>
2026-06-30 11:30:18 +0500

    arithmetic operator was added

---
+++ operators/arithmetic.py
@@ -0,0 +1,21 @@
+# Operator	            Name	                Example	
+# +	                    Addition	            x + y	
+# -	                    Subtraction	            x - y	
+# *	                    Multiplication	        x * y	
+# /	                    Division	            x / y	
+# %	                    Modulus	                x % y	
+# **	                Exponentiation	        x ** y	
+# //	                Floor division	        x // y
+
+
+x = 9
+y = 2
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y) # qoldigini chiqaradi
+print(x ** y) # darajaga kotaradi sqrt ga kabi
+print(x // y) # butun qismini chiqaradi
+
