d0b608def3af94ee5f461343964901051492e775
muhammadrizo <muhammadrizo42maktab@gmail.com>
2026-06-30 11:53:23 +0500

    ternary operator was added

---
+++ operators/ternary_operator.py
@@ -0,0 +1,10 @@
+# ternar operatori tekshirish natijasida biror o'zgaruvchiga qiymat berish uchun ishlatiladi
+num  = 6
+x = "WEEKEND!" if num > 5 else "Workday"
+print(x)
+
+# Ternar operator haqiqiy operator emas balki conditional expression yoki if statementni qisqa korinishi
+
+
+# ternar operatorini elif ni orniga yani uzun operationlarda ham ishlatsa boladi
+x = "Fri" if x == 5 else "Sat" if num == 6 else "Sun" if num == 7 else "weekday"
