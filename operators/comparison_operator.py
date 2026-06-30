b64f097a047892d128436529b57242aace6acc6e
muhammadrizo <muhammadrizo42maktab@gmail.com>
2026-06-30 11:58:19 +0500

    comparison operator was added

---
+++ operators/comparison_operator.py
@@ -0,0 +1,19 @@
+# Solishtirish (comparison) operatorlar 2 ta value ni solishitirish uchun ishlatilinadi
+
+x = 5
+y = 3
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+
+
+
+# Python comparison operatorlarni zanjir sifatida ishlatish imkonini beradi
+x = 5
+print(1 < x < 10)
+print(1 < x and x < 10)
